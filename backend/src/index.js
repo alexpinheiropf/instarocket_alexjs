1bf7ddc4c9065c405fcaacf11513728a0e1e29c7
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-06-19 00:15:00 -0300

    Update index.js
    Test backend application (NodeJs)

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { //interceptando a rota raiz
-    return res.send(`Olá ${req.query.name}`);
+    return res.send(`Hello ${req.query.name}`);
 });
 
 app.listen(3333);//ouvir a porta
