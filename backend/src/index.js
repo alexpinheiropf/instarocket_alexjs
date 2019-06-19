e5f8291a70a9230c4a2e3f877d150572e4188b64
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-06-19 00:19:42 -0300

    Comment Hello World

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { //interceptando a rota raiz
-    return res.send(`Hello ${req.query.name}`);
+    return res.send(`Hello ${req.query.name}`);//teste Hello World
 });
 
 app.listen(3333);//ouvir a porta
