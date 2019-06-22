5c78b268b8c7e4bfdc9bc0158c1b78e5e6f653da
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-06-22 14:43:20 -0300

    Sending request via Post, doing GET search and rendering the image to 500 px excluding the larger image

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,15 +1,41 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     //Rota de listagem de posts
-    async indexedDB(req, res){
-
+    async index(req, res){
+        const posts = await Post.find().sort('-createdAt');
+        return res.json(posts);
     },
 
     //Cadastrar novos posts 
     async store(req, res){
-        console.log(req.body);//teste de retorno
+        const { author, place, description, hashtags} = req.body;
+        const { filename: image } = req.file;
 
-        return res.json({ok : true});
+        //Separar o nome da varial com o ponto (.)
+        const [ name ] = image.split('.');
+        const filename = `${name};jpg`;
+
+        await sharp(req.file.path)
+            .resize(500) //Tamanho de 500px
+            .jpeg({ quality: 70})//Qualidade em 70%
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            )
+        
+        fs.unlinkSync(req.file.path);
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        });
+
+        return res.json(post);
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');//importar express
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();//cria a aplicação chamando o express (criando o servidor)
 
 mongoose.connect('mongodb+srv://office:office@cluster0-2ldqo.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
 })
+//referenciar algum arquivo dentro da pasta
+app.user('/files', express.static(path.resolve(__dirname, '..', 'upload', 'resized')));
 
 app.use(require('./routes'));
 
