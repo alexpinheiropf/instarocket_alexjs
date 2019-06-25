3ad89d304f097a0872aa8bddc077bb890cf02a39
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-06-25 00:00:37 -0300

    Criando Links e rotas da primeira pagina (Feed)

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Feed.css';
+
+class Feed extends Component {
+    render () {
+        return(
+            <h1>Feed</h1>
+        );
+    }
+}
+
+export default Feed;
