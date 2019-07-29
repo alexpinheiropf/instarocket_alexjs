40c2f359baa63d95920dbae26ad9f99511476120
Alex Pinheiro <alexpinheiropf@hotmail.com>
2019-07-29 20:25:31 -0300

    implemented news and mobile feed

---
+++ Pixel_3_XL_API_29.bat
@@ -0,0 +1,2 @@
+cd C:\Users\alexp\AppData\Local\Android\Sdk\emulator
+emulator -avd Pixel_3_XL_API_29
--- mobile/src/pages/Feed.js
+++ mobile/src/pages/Feed.js
@@ -32,7 +32,7 @@ export default class Feed extends Component {
   }
 
   registerToSocket = () => {
-    const socket = io('http://10.0.2.2:3333');
+    const socket = io('http://192.168.0.105:3333');
 
     // post, like
 
